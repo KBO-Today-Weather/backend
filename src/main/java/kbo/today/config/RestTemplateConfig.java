1fd686661d04976f17310c56618455483028907b
hangyeol <snake011128@naver.com>
2026-02-16 15:09:56 +0900

    fix: RestTemplate timeout 설정

--- src/main/java/kbo/today/config/RestTemplateConfig.java
+++ src/main/java/kbo/today/config/RestTemplateConfig.java
@@ -2,14 +2,23 @@ package kbo.today.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RestTemplateConfig {
 
+    private static final int CONNECT_TIMEOUT_SECONDS = 3;
+    private static final int READ_TIMEOUT_SECONDS = 10;
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) TimeUnit.SECONDS.toMillis(CONNECT_TIMEOUT_SECONDS));
+        factory.setReadTimeout((int) TimeUnit.SECONDS.toMillis(READ_TIMEOUT_SECONDS));
+        return new RestTemplate(factory);
     }
 }
 
